cdb46cd0ada97e9a7e44f57aad156e0bae8aab7f
babywithaweed <babywithaweed@users.noreply.github.com>
2020-12-30 16:27:15 +0530

    first bot

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,8 +1,8 @@
 module.exports = async (client) =>{
-    const guild = client.guilds.cache.get('778855533196476436');
+    const guild = client.guilds.cache.get('745570552869027860');
     setInterval(() =>{
         const memberCount = guild.memberCount;
-        const channel = guild.channels.cache.get('778855533196476440');
+        const channel = guild.channels.cache.get('793794385896669195');
         channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
         console.log('Updating Member Count');
     }, 600000);
