6466344d91856e17695ed2b6cf811a64139cf131
babywithaweed <babywithaweed@users.noreply.github.com>
2021-01-24 10:20:32 +0530

    github
    update

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -5,5 +5,5 @@ module.exports = async (client) =>{
         const channel = guild.channels.cache.get('793794385896669195');
         channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
         console.log('Updating Member Count');
-    }, 1000);
+    }, 100);
 }
